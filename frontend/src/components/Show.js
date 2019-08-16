359925380ed14e893170c2497f086fed2f5a7b3c
davidlinke <davidlinke@davidlinke.com>
2019-08-16 08:53:09 -0400

    added empty state before any bookmarks are added

--- frontend/src/components/Show.js
+++ frontend/src/components/Show.js
@@ -4,37 +4,43 @@ class Show extends React.Component {
 	render() {
 		return (
 			<div className='bookmarkOuter'>
-				<div className='bookmarkGraphicUpper'>
-					{this.props.bookmarks.map(bookmark => {
-						return (
-							<div className='bookmark' key={bookmark._id}>
-								<a
-									target='_blank'
-									rel='noopener noreferrer'
-									href={bookmark.url}
-								>
-									<p className='inline'> {bookmark.name}</p>
-								</a>
-								<div className='bookmarkMenu'>
-									<p
-										className='inline edit'
-										// onClick={() => this.props.deleteBookmark(bookmark._id)}
-									>
-										edit
-									</p>
-									<p className='inline divider'>/</p>
-									<p
-										className='inline delete'
-										onClick={() => this.props.deleteBookmark(bookmark._id)}
-									>
-										delete
-									</p>
-								</div>
-							</div>
-						);
-					})}
-				</div>
-				<div className='bookmarkGraphicLower' />
+				{this.props.bookmarks.length ? (
+					<div>
+						<div className='bookmarkGraphicUpper'>
+							{this.props.bookmarks.map(bookmark => {
+								return (
+									<div className='bookmark' key={bookmark._id}>
+										<a
+											target='_blank'
+											rel='noopener noreferrer'
+											href={bookmark.url}
+										>
+											<p className='inline'> {bookmark.name}</p>
+										</a>
+										<div className='bookmarkMenu'>
+											<p
+												className='inline edit'
+												// onClick={() => this.props.deleteBookmark(bookmark._id)}
+											>
+												edit
+											</p>
+											<p className='inline divider'>/</p>
+											<p
+												className='inline delete'
+												onClick={() => this.props.deleteBookmark(bookmark._id)}
+											>
+												delete
+											</p>
+										</div>
+									</div>
+								);
+							})}
+						</div>
+						<div className='bookmarkGraphicLower' />
+					</div>
+				) : (
+					<p>Add a bookmark to get started!</p>
+				)}
 			</div>
 		);
 	}
